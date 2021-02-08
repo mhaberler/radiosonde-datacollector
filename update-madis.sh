104ea0cf6a973bc29c6efc3b04d83e03d769348e
Michael Haberler <git@mah.priv.at>
2021-02-08 09:13:43 +0100

    MADIS ftp mirror script

---
+++ update-madis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+REMOTE_HOST="madis-data.ncep.noaa.gov"
+REMOTE_DIR=point/raob/netcdf/
+LOCAL_DIR=var/spool/madis
+USER=anonymous
+PASS=user@example.com
+MAIL=user@example.com
+
+TMP=$(mktemp)
+var=$(lftp -u $USER,$PASS -e "mirror --parallel=2 --verbose /$REMOTE_DIR /$LOCAL_DIR; bye" $REMOTE_HOST 2> "$TMP")
+err=$(cat "$TMP")
+
+
+if test -n "$err"
+      echo "stderr=$err stdout=$var" | mail -s 'madis update failed' $MAIL
+fi
+
+rm "$TMP"
