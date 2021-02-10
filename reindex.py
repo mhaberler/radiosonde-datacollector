64f84e239e68846bf1806d2922db85b31e4f52d1
Michael Haberler <git@mah.priv.at>
2021-02-10 19:52:25 +0100

    update totals

--- reindex.py
+++ reindex.py
@@ -23,6 +23,7 @@ def walkt_tree(directory, pattern):
 
 def  main(dirlist):
     ntotal = 0
+    ttotal = 0
     for d in dirlist:
         start = time.time()
         nf, nu, nc = walkt_tree(pathlib.Path(d),'*.geojson.br')
@@ -31,11 +32,13 @@ def  main(dirlist):
             continue
         end = time.time()
         dt = end-start
+        ttotal += dt
         ratio = (1. - nu/nc)*100.
         print(f"directory {d}:")
         print(f"{nf} files, avg compressed file={nc/nf:.0f},  avg uncompressed file={nu/nf:.0f}")
         print(f"avg compression={ratio:.1f}%, total time {dt:.3f}s  {dt*1000/nf:.3f}ms per file")
-    print(f"files in total {ntotal}")
+
+    print(f"read {ntotal} files in {ttotal:.3f}s, avg {ttotal*1000/ntotal:.3f}ms per file")
 
 if __name__ == "__main__":
     dirlist = [MADIS, GISC]
