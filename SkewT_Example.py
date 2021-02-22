0f227661ddacbe5e1df5e2e18cb0a137339e4a39
Michael Haberler <git@mah.priv.at>
2021-02-22 13:00:05 +0100

    metpy SkewT example using radiosonde archive

--- SkewT_Example.py
+++ SkewT_Example.py
@@ -2,13 +2,11 @@
 Skew-T Analysis
 ===============
 
-Classic skew-T/log-p plot using data from University of Wyoming.
+Classic skew-T/log-p plot using data the radiosonde.mah.priv.at archive.
 
-This example uses example data from the University of Wyoming sounding
-archive for 12 UTC 31 October 2016 for Minneapolis, MN (MPX) and uses
-MetPy to plot the classic skew-T with Temperature, Dewpoint, and wind
-barbs.
-
+This example retrieves the temp for Vienna/Hohe Warte (Austria) of
+2021-2-21 12:00Z and creates a MetPy skew-T plot with temperature,
+dewpoint, and wind barbs.
 """
 
 from datetime import datetime
@@ -17,39 +15,34 @@ import matplotlib.pyplot as plt
 from metpy.plots import SkewT
 from metpy.units import pandas_dataframe_to_unit_arrays, units
 import numpy as np
-# from siphon.simplewebservice.wyoming import WyomingUpperAir
 
 from radiosonde import Station
+from pprint import pprint
 
 ######################################################################
 # Set time using a datetime object and station as variables
 #
 
-dt = datetime(2021, 2, 18, 12)
-dt = datetime(2021, 2, 21, 12)
-station = 'Wien/Hohe Warte'
+dt = datetime(2021, 2, 22, 00)
+
+# use WMo id or exact name from station_list.json:
 #station = "11035"
+station = 'Wien/Hohe Warte'
+dt = datetime(2021, 2, 22, 3)
 
+station = "11240"
 st = Station(station)
-# for syn_time, src, fn in st.available():
-#     print(syn_time, src, fn)
-data = st.as_dataframe(0) #date=dt)
 
-######################################################################
-# Grab Remote Data
-# ----------------
-#
-# This requires an internet connection to access the sounding data from a
-# remote server at the University of Wyoming.
-#
+print(f"available ascents for station {station}:")
+for syn_time, src, fn in st.available():
+    print(syn_time, src, fn)
 
-# Read remote sounding data based on time (dt) and station
-#df = WyomingUpperAir.request_data(dt, station)
+# 'data' is a pandas.DataFrame with a Metpy units dictionary attached:
+data, metadata = st.as_dataframe(date=dt)
 
-# Create dictionary of united arrays
-#data = pandas_dataframe_to_unit_arrays(df)
-# import sys
-# sys.exit(0)
+# 'metadata' is additional information about the ascent and the data source:
+print(f"ascent metadata for station {station} date {dt}:")
+pprint(metadata)
 
 ######################################################################
 # Isolate variables and attach units
@@ -92,9 +85,13 @@ skew.plot_dry_adiabats(t0=np.arange(233, 533, 10) * units.K,
                        alpha=0.25, color='orangered')
 skew.plot_moist_adiabats(t0=np.arange(233, 400, 5) * units.K,
                          alpha=0.25, color='tab:green')
+
+# does not work for me, unclear why:
 # skew.plot_mixing_lines(p=np.arange(1000, 99, -20) * units.hPa,
 #                         linestyle='dotted', color='tab:blue')
+# this does:
 skew.plot_mixing_lines(linestyle='dotted', color='tab:blue')
+
 # Add some descriptive titles
 plt.title('{} Sounding'.format(station), loc='left')
 plt.title('Valid Time: {}'.format(dt), loc='right')
