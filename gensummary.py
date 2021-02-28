44a48e0d75433f72ec1d12bfc385a5a4e6df75e8
Michael Haberler <git@mah.priv.at>
2021-02-28 10:49:01 +0100

    gensummary

--- gensummary.py
+++ gensummary.py
@@ -8,10 +8,14 @@ import ciso8601
 from datetime import datetime
 import pytz
 import json
-from config import STATION_LIST
+from config import STATION_LIST, MAX_ASCENT_AGE_IN_SUMMARY
 from geojson import Feature, FeatureCollection, Point
 from pprint import pprint
 import brotli
+import re
+from operator import itemgetter
+import reverse_geocode
+
 '''
 
 generate summary JSON file of all files under MADIS and GISC
@@ -44,6 +48,8 @@ GISC = r'/var/www/radiosonde.mah.priv.at/data-dev/gisc'
 
 flights = {}
 
+def now():
+    return datetime.utcnow().timestamp()
 
 def read_br(path):
     with open(path, mode='rb') as f:
@@ -53,7 +59,7 @@ def read_br(path):
         return geojson.loads(s.decode())
 
 
-def walkt_tree(toplevel, directory, pattern):
+def walkt_tree(toplevel, directory, pattern, after):
     nf = 0
     nc = 0
     nu = 0
@@ -63,6 +69,11 @@ def walkt_tree(toplevel, directory, pattern):
             s = s.rsplit(".", 1)[0]
         stid, day, tim = s.split('_')
         ts = ciso8601.parse_datetime(day + " " + tim + "-00:00").timestamp()
+
+        if ts < after:
+            #print("skipping", s, file=sys.stderr)
+            continue
+
         #print(stid, day, tim, datetime.fromtimestamp(ts, pytz.utc))
         if toplevel.endswith('madis'):
             typus = "netCDF"
@@ -74,15 +85,26 @@ def walkt_tree(toplevel, directory, pattern):
         }
         gj = None
         if stid not in station_list:
-            # mobile. Use per-ascent coordinates and
-            # propagate down into entry.
-            gj = read_br(p)
-            typus = gj.properties['id_type']
-            entry['lat'] = gj.properties['lat']
-            entry['lon'] = gj.properties['lon']
-            entry['elevation'] = gj.properties['elevation']
+            # maybe mobile. Check ascent for type
+            # example unregistered, but obviously fixed:
+            # https://radiosonde.mah.priv.at/data-dev/gisc/08/383/08383_20210206_120000.geojson
+            # check id syntax - 5 digits = unregistered else mobile
+            if re.match(r"^\d{5}$",stid):
+                # WMO id syntax, but not in station_list
+                # hence an unregistered but fixed station
+                idtype = 'unregistered'
+            else:
+                # could be ship registration syntax. Check detail file.
+                gj = read_br(p)
+                idtype = gj.properties['id_type']
+                # propagate per-ascent coords down to ascent
+                entry['lat'] = round(gj.properties['lat'], 6)
+                entry['lon'] = round(gj.properties['lon'], 6)
+                entry['elevation'] = round(gj.properties['elevation'],2)
         else:
+            # registered
             st = station_list[stid]
+            idtype = 'wmo'
 
         if stid not in flights:
             flights[stid] = Feature(
@@ -95,35 +117,45 @@ def walkt_tree(toplevel, directory, pattern):
 
         f = flights[stid]
         f.properties['station_id'] = stid
+        f.properties['id_type'] = idtype
+        f.properties['name'] = stid
 
         if stid in station_list:
             st = station_list[stid]
-            f.properties['id_type'] = "wmo"
+            # override name if we have one
             f.properties['name'] = st['name']
-            f.geometry = Point((round(st["lon"], 6),
-                                round(st["lat"], 6),
-                                round(st["elevation"], 1)))
         else:
-            gj = read_br(p)
-            # FIXME this should be from file contents
-            f.properties['id_type'] = "mobile"
-            f.properties['name'] = stid
-
+            if idtype == 'unregistered':
+                loc = reverse_geocode.get((st["lat"], st["lon"]))
+                if loc:
+                    f.properties['name'] = loc['city'] + ", " + loc['country_code']
+                #print(loc, file=sys.stderr)
+        # this needs fixing up for mobiles after sorting
+        f.geometry = Point((round(st["lon"], 6),
+                            round(st["lat"], 6),
+                            round(st["elevation"], 1)))
         nf += 1
     return (nf, 1, 1)
 
-    # "05467": {
-    #     "name": "Sharjah",
-    #     "lat": 25.25,
-    #     "lon": 55.37,
-    #     "elevation": 4.0
-    # },
-    # if stid in station_list:
-    #     kind = "wmo"
-    #     name = station_list[stid].name
+def fixup_flights(flights):
+    # pass 1: reverse sort ascents by timestamp
+    for stid, f in flights.items():
+        a = f.properties['ascents']
+        f.properties['ascents'] = sorted(a,
+                                         key=itemgetter("syn_timestamp"),
+                                         reverse=True)
 
+    # pass 2: for mobile stations, propagate up
+    # coords of newest ascent to geometry.coords
+    for stid, f in flights.items():
+        if f.properties['id_type'] == 'mobile':
+            latest = f.properties['ascents'][0]
+            f.geometry = Point((round(latest["lon"], 6),
+                                round(latest["lat"], 6),
+                                round(latest["elevation"], 1)))
 
 def main(dirlist):
+    cutoff_ts = now() - MAX_ASCENT_AGE_IN_SUMMARY
     global station_list
     with open(STATION_LIST) as json_file:
         station_list = json.load(json_file)
@@ -131,22 +163,10 @@ def main(dirlist):
     ttotal = 0
     for d in dirlist:
         start = time.time()
-        nf, nu, nc = walkt_tree(d, pathlib.Path(d), '*.geojson.br')
+        nf, nu, nc = walkt_tree(d, pathlib.Path(d), '*.geojson.br', cutoff_ts)
         ntotal = ntotal + nf
-        if nf == 0:
-            continue
-        end = time.time()
-        dt = end - start
-        ttotal += dt
-        ratio = (1. - nu / nc) * 100.
-        print(f"directory {d}:")
-        print(
-            f"{nf} files, avg uncompressed file={nc/nf:.0f},  avg compressed file={nu/nf:.0f}")
-        print(
-            f"avg compression={ratio:.1f}%, total time {dt:.3f}s  {dt*1000/nf:.3f}ms per file")
 
-    print(
-        f"read {ntotal} files in {ttotal:.3f}s, avg {ttotal*1000/ntotal:.3f}ms per file")
+    fixup_flights(flights)
     pprint(flights)
 
 
