d9796d128824371c773ce70dea780c8a516b1942
Michael Haberler <git@mah.priv.at>
2021-03-01 19:25:27 +0100

    generate bboxes of all flights

--- reindex.py
+++ reindex.py
@@ -3,45 +3,112 @@ import brotli
 import geojson
 import time, sys, os
 
+import pandas as pd
+from vincenty import vincenty
+from pprint import pprint
+import geopandas
+
+
 MADIS = r'/var/www/radiosonde.mah.priv.at/data-dev/madis'
 GISC = r'/var/www/radiosonde.mah.priv.at/data-dev/gisc'
 
+def latlon(f):
+    return (f['geometry']['coordinates'][1],
+            f['geometry']['coordinates'][0])
+
+#12374 [(51.71204, 20.90611), (52.81019, 23.51059)]
+def dump_bboxes(points, filename):
+    fc = geojson.FeatureCollection([])
+
+
+    for st, pts in points.items():
+        if len(pts) == 0:
+            continue
+
+        lbot,rtop = bounding_box_naive(pts)
+        llon, llat = lbot
+        rlon, rlat = rtop
+        print(st, lbot,rtop)
+
+        f = geojson.Feature(geometry=geojson.MultiLineString([[(llat,llon), (rlat,llon), (rlat,rlon),(llat,rlon),(llat,llon)]]),
+                            properties= {
+                                'station_id': st
+                            })
+        fc.features.append(f)
+    with open(filename, "wb") as jfile:
+        gj = geojson.dumps(fc, indent=4).encode("utf8")
+        jfile.write(gj)
+
+
+def bounding_box_naive(points):
+    bot_left_x = min(point[0] for point in points)
+    bot_left_y = min(point[1] for point in points)
+    top_right_x = max(point[0] for point in points)
+    top_right_y = max(point[1] for point in points)
+
+    return [(bot_left_x, bot_left_y), (top_right_x, top_right_y)]
+
+points = {}
+
+def extent(gj):
+    if gj.properties['id_type'] != "wmo":
+        return
+    stid = gj.properties['station_id']
+    if stid not in points:
+        points[stid] = []
+
+    takeoff = latlon(gj.features[0])
+    maxd = 0
+    pts = []
+
+    for f in gj.features:
+        cp = latlon(f)
+        d = vincenty(takeoff, cp, miles=False)
+        if not d:
+            continue
+        pts.append(cp)
+        if d > maxd:
+            maxd = d
+
+    points[stid].extend(pts)
+    if d < 0.1:
+        return None
+    return gj.properties['station_id'], d
+
+flights = []
 def walkt_tree(directory, pattern):
     nf = 0
     nc = 0
     nu = 0
     for path in sorted(directory.rglob(pattern)):
+        #print(path, file=sys.stderr)
         with open(path, mode='rb') as f:
             s = f.read()
             nu += len(s)
             if path.suffix == '.br':
                 s = brotli.decompress(s)
                 nc += len(s)
-            summary = geojson.loads(s.decode())
+            gj = geojson.loads(s.decode())
             nf += 1
-    return (nf,nu,nc)
+            r = extent(gj)
+            if r:
+                flights.append(r)
+    return nf
 
 def  main(dirlist):
-    ntotal = 0
-    ttotal = 0
+    nf = 0
     for d in dirlist:
-        start = time.time()
-        nf, nu, nc = walkt_tree(pathlib.Path(d),'*.geojson.br')
-        ntotal = ntotal + nf
-        if nf == 0:
-            continue
-        end = time.time()
-        dt = end-start
-        ttotal += dt
-        ratio = (1. - nu/nc)*100.
-        print(f"directory {d}:")
-        print(f"{nf} files, avg compressed file={nc/nf:.0f},  avg uncompressed file={nu/nf:.0f}")
-        print(f"avg compression={ratio:.1f}%, total time {dt:.3f}s  {dt*1000/nf:.3f}ms per file")
+        nf += walkt_tree(pathlib.Path(d),'*.geojson.br')
 
-    print(f"read {ntotal} files in {ttotal:.3f}s, avg {ttotal*1000/ntotal:.3f}ms per file")
+    dump_bboxes(points, "flight-bbox.geojson")
+
+
+    f = sorted(flights, key=lambda tup: tup[1])
+    print(f"{nf} flights")
+    pprint(f)
 
 if __name__ == "__main__":
-    dirlist = [MADIS, GISC]
-    #dirlist = ['gisc/', 'madis/']
+    dirlist = [GISC]
+    #dirlist = ['gisc/']
 
     sys.exit(main(dirlist))
