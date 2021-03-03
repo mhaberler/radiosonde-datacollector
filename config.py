9930036a8a8e3580adb3cc0d0c7871275b07eb01
Michael Haberler <git@mah.priv.at>
2021-03-03 21:06:14 +0100

    WIP
    # Conflicts:
    #	config.py
    #	process.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ MAX_FLIGHT_DURATION = 3600 * 5  # rather unlikely
 
 # see examples/polyfit.py
 # y = 6.31426 * x + -0.00019 * x^2 + -373.12281
-ASCENT_RATE = 6.3 # m/s = ca 380m/min
+ASCENT_RATE = 6.3  # m/s = ca 380m/min
 
 BROTLI_SUMMARY_QUALITY = 11  # 7
 
@@ -12,9 +12,18 @@ BROTLI_SUMMARY_QUALITY = 11  # 7
 # the files are kept nevertheless
 MAX_DAYS_IN_SUMMARY = 14
 
-SPOOLDIR_MADIS = r"/var/spool/madis/"
-SPOOLDIR_GISC = r"/var/spool/gisc/"
-SPOOLDIR_GISC_TOKYO = r"/var/spool/gisc-tokyo/"
+# after 3 days move to processed
+KEEP_MADIS_PROCESSED_FILES = 86400 * 3
+
+# added to featurecollection.properties.fmt and summary.fmt
+FORMAT_VERSION = 5
+
+WWW_DIR = "/var/www/radiosonde.mah.priv.at/"
+DATA_DIR = "data/"
+STATIC_DIR = "static/"
+STATION_TXT = "station_list.txt"
+SUMMARY = "summary.geojson.br"
+
 PROCESSED = r"processed"
 FAILED = r"failed"
 INCOMING = r"incoming"
@@ -22,20 +31,13 @@ TS_PROCESSED = ".processed"
 TS_FAILED = ".failed"
 TS_TIMESTAMP = ".timestamp"
 LOCKFILE = "/var/lock/process-radiosonde.pid"
-DATA_DIR = "data/"
-STATIC_DIR = "static/"
-WWW_DIR = "/var/www/radiosonde.mah.priv.at/"
 STATION_LIST = WWW_DIR + STATIC_DIR + "station_list.json"
 MADIS_DATA = WWW_DIR + DATA_DIR + "madis/"
 GISC_DATA = WWW_DIR + DATA_DIR + "gisc/"
-STATION_TXT = "station_list.txt"
 CHARSET = "utf-8"
 tmpdir = "/tmp"
 INDENT = 4
-SUMMARY = "summary.geojson.br"
-
-# after 3 days move to processed
-KEEP_MADIS_PROCESSED_FILES = 86400 * 3
+MESSAGE_START_SIGNATURE = b'BUFR'
 
 # added to featurecollection.properties.fmt = FORMAT_VERSION
 # 4 - using deep subdirs year/month under station
@@ -60,3 +62,35 @@ TAG_FM35 = True
 # hires BUFR files have a lot of samples
 HSTEP = 100
 
+SPOOLDIR = r"/var/spool/"
+SPOOLDIR_NOAA_MADIS = SPOOLDIR + f"madis/"
+SPOOLDIR_GISC_OFFENBACH = SPOOLDIR + r"gisc-offenbach/"
+SPOOLDIR_GISC_TOKYO = SPOOLDIR + r"gisc-tokyo/"
+SPOOLDIR_GISC_MOSCOW = SPOOLDIR + r"gisc-moscow/"
+
+channels = {
+    "gisc-offenbach": {
+        "name":  "GISC Offenbach",
+        "spooldir": SPOOLDIR_GISC_OFFENBACH,
+        "pattern":  "*.zip",
+        "housekeeping": "standard",
+    },
+    "gisc-moscow": {
+        "name":  "GISC Moscow",
+        "spooldir": SPOOLDIR_GISC_MOSCOW,
+        "housekeeping": "standard",
+        "pattern":  "*.bufr",
+    },
+    "gisc-tokyo": {
+        "name":  "GISC Tokyo",
+        "spooldir": SPOOLDIR_GISC_TOKYO,
+        "housekeeping": "standard",
+        "pattern":  "*.bufr",
+    },
+    "noaa-madis": {
+        "name":  "NOAA MADIS",
+        "spooldir": SPOOLDIR_NOAA_MADIS,
+        "housekeeping": "delayed",
+        "pattern":  "*.gz",
+    },
+}
